9d576264af72a4f60475487270b169c33acc9acf
ChriVasi <chrivasi1@uth.gr>
2023-12-14 20:47:13 +0200

    show-map

--- app/src/main/java/local/business/discovery/MainActivity.java
+++ app/src/main/java/local/business/discovery/MainActivity.java
@@ -1,35 +1,65 @@
 package local.business.discovery;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.SearchView;
 
+import com.caverock.androidsvg.BuildConfig;
+
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.BoundingBox;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     private BusinessAdapter businessAdapter;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (isFirstLaunch()) {
-            startIntroActivity();
+        // Check if GPS is enabled
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // GPS is not enabled, prompt the user to enable it
+            buildAlertMessageNoGps();
             return;
         }
+
+        // Check if internet connection is available
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        if (activeNetwork == null || !activeNetwork.isConnectedOrConnecting()) {
+            // Internet is not available, prompt the user to enable it
+            buildAlertMessageNoInternet();
+            return;
+        }
+
+        // Initialize osmdroid
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
 
         setContentView(R.layout.activity_main);
 
@@ -45,19 +75,50 @@ public class MainActivity extends AppCompatActivity {
         businessAdapter = new BusinessAdapter(new ArrayList<>());
         recyclerView.setAdapter(businessAdapter);
 
+        // Initialize MapView
+        mapView = findViewById(R.id.mapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getController().setZoom(10.0); // Set a default zoom level (adjust as needed)
+        mapView.setMultiTouchControls(true);
+
+        // Enable my location
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(mapView);
+        mapView.getOverlays().add(myLocationOverlay);
+        myLocationOverlay.enableMyLocation();
+
         // Load businesses dynamically
         loadBusinesses();
     }
 
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled. Do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)))
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel());
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void buildAlertMessageNoInternet() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your internet connection seems to be disabled. Do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS)))
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel());
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     private void loadBusinesses() {
-        // Assuming user credentials are saved in SharedPreferences after successful login
+        // Assuming user credentials are saved in SharedPreferences after a successful login
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         String userEmail = sharedPref.getString(getString(R.string.key_email), "");
 
         // Replace this with your logic to load businesses for the logged-in user
         // For testing, you can still add hardcoded businesses
         List<Business> businesses = new ArrayList<>();
-        businesses.add(Business.newBuilder("Local Cafe", "Coffee & Pastries", "123 Main St", 37.7749, -122.4194)
+        businesses.add(Business.newBuilder("Local Souvlaki", "Souvlaki & Pastries", "Agias Paraskevis 17", 39.37913, 22.95989)
                 .addReview(new Review("User1", 4.5, "Great place"))
                 .addReview(new Review("User2", 5.0, "Excellent service"))
                 .build());
@@ -66,6 +127,45 @@ public class MainActivity extends AppCompatActivity {
 
         // Update the adapter with the new list of businesses
         businessAdapter.updateData(businesses);
+
+        // Display markers on the map
+        displayMarkersOnMap(businesses);
+    }
+
+    private void displayMarkersOnMap(List<Business> businesses) {
+        List<IGeoPoint> geoPoints = businesses.stream()
+                .map(business -> new GeoPoint(business.getLatitude(), business.getLongitude()))
+                .collect(Collectors.toList());
+
+        BoundingBox boundingBox = BoundingBox.fromGeoPoints(geoPoints);
+
+        // Calculate the zoom level based on the bounding box
+        int padding = 50; // You can adjust this padding based on your preference
+        int zoomLevel = calculateZoomLevel(boundingBox, mapView.getWidth() - 2 * padding, mapView.getHeight() - 2 * padding);
+
+        // Zoom to the bounding box with the calculated zoom level
+        mapView.zoomToBoundingBox(boundingBox, true, zoomLevel);
+
+        for (Business business : businesses) {
+            GeoPoint geoPoint = new GeoPoint(business.getLatitude(), business.getLongitude());
+            CustomMarker marker = new CustomMarker(mapView);
+            marker.setPosition(geoPoint);
+            marker.setTitle(business.getName());
+            mapView.getOverlays().add(marker);
+        }
+
+        mapView.invalidate(); // Refresh the map
+    }
+
+    private int calculateZoomLevel(BoundingBox boundingBox, int width, int height) {
+        final int border = 20;
+        double diffLat = boundingBox.getLatNorth() - boundingBox.getLatSouth();
+        double diffLon = boundingBox.getLonEast() - boundingBox.getLonWest();
+
+        double zoomLat = Math.log((height - 2 * border) / 256.0 / diffLat) / Math.log(2);
+        double zoomLon = Math.log((width - 2 * border) / 256.0 / diffLon) / Math.log(2);
+
+        return (int) Math.min(zoomLat, zoomLon);
     }
 
     private boolean isFirstLaunch() {
@@ -110,4 +210,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Custom Marker class for marker customization
+    private static class CustomMarker extends Marker {
+        public CustomMarker(MapView mapView) {
+            super(mapView);
+            // Customize the marker appearance here, e.g., setIcon, setAnchor, etc.
+        }
+    }
 }
